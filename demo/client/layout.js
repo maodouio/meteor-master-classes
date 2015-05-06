3b340a5043a25188ab53f14e8037a0a52b05de68
谭旭 <tanxu@tanxudeMacBook-Pro.local>
2015-05-06 16:04:16 +0800

    Add Project

---
+++ demo/client/layout.js
@@ -0,0 +1,37 @@
+Template.layout.events({
+  'click [data-action=showActionSheet]': function (event, template) {
+    var buttons = [];
+    if (Meteor.user() != null) {
+      buttons.push({ id: 'addPost', text: 'New Post <i class="icon ion-ios-compose"></i>' });
+      buttons.push({ id: 'logout', text: 'Logout <i class="icon ion-log-out"></i>' });
+    } else {
+      buttons.push({ id: 'login', text: 'Login <i class="icon ion-log-in"></i>' });
+    }
+    IonActionSheet.show({
+      buttons: buttons,
+      cancelText: 'Cancel',
+      cancel: function() {
+        console.log('Cancelled!');
+      },
+      buttonClicked: function(index) {
+        switch(buttons[index].id)
+        {
+          case 'addPost':
+            Router.go('/addPost');
+            break;
+          case 'login':
+            Router.go('/userAccounts');
+            break;
+          case 'logout':
+            Meteor.logout(function() {
+              toastr.success('Logged out');
+              return Router.go('lists');
+            });
+            break;
+          default:
+        }
+        return true;
+      }
+    });
+  }
+});
---
+++ demo/client/templates/addPost/addPost.js
@@ -0,0 +1,12 @@
+AutoForm.setDefaultTemplate('ionic');
+Template.addPost.events({
+  'click a.ion-android-send': function(e, t) {
+    var $title = t.find('#title'), $body = t.find('#body'), newPost, postId;
+    newPost = {
+      title: $title.value ? $title.value : '',
+      body: body ? $body.value : ''
+    };
+    postId = Posts.insert(newPost);
+    return Router.go("/postView/" + postId);
+  }
+});
